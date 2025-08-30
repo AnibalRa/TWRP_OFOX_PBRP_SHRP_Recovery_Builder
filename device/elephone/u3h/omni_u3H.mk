8530bd670dbc62607f54ba03d785ff5219620312
AnibalRa <anbl.tmp@gmail.com>
2025-08-30 21:39:15 +0200

    Add files via upload

---
+++ device/elephone/u3h/omni_u3H.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from u3h device
+$(call inherit-product, device/elephone/u3h/device.mk)
+
+# Inherit from the common full phone product configuration.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_phone.mk)
+
+# Product characteristics
+PRODUCT_NAME := omni_u3h
+PRODUCT_DEVICE := u3h
+PRODUCT_BRAND := elephone
+PRODUCT_MANUFACTURER := elephone
+PRODUCT_MODEL := Elephone U3H
+
+PRODUCT_GMS_CLIENTID_BASE := android-elephone
